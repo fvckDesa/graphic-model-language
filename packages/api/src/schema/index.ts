f51ccfef157e4af98439d52491db904d94f88af2
Luca De Santis <lucaroma2705@gmail.com>
2023-09-24 16:25:17 +0200

    feat: add native enum to enumProperty value

--- packages/api/src/schema/index.ts
+++ packages/api/src/schema/index.ts
@@ -6,6 +6,7 @@ import {
   TypeGuard,
   TOmit,
   TPick,
+  TComposite,
 } from "@sinclair/typebox";
 import { Property } from "./properties";
 export * from "./properties";
@@ -29,9 +30,17 @@ export function schema<P extends Properties>({
 export type SubSchema<P extends Properties = Properties> = TArray<Schema<P>>;
 
 export function subSchema<P extends Properties>(
+  schema: Schema<P>
+): SubSchema<P>;
+export function subSchema<P extends Properties>(
   options: SchemaOptions<P>
+): SubSchema<P>;
+export function subSchema<P extends Properties>(
+  schemaOrOptions: Schema<P> | SchemaOptions<P>
 ): SubSchema<P> {
-  return Type.Array(schema(options));
+  return Type.Array(
+    isSchema(schemaOrOptions) ? schemaOrOptions : schema(schemaOrOptions)
+  );
 }
 
 export type State<S extends Schema> = Static<S>;
@@ -51,3 +60,27 @@ export const omit = Type.Omit.bind(Type);
 
 export type SchemaPick<S extends Schema> = TPick<S, string>;
 export const pick = Type.Pick.bind(Type);
+
+export type SchemaMerge<S extends Schema[]> = TComposite<S>;
+export interface MergeOptions<S extends Schema[]> {
+  name?: string;
+  schemas: [...S];
+}
+
+export function merge<S extends Schema[]>(schemas: [...S]): SchemaMerge<S>;
+export function merge<S extends Schema[]>(
+  options: MergeOptions<S>
+): SchemaMerge<S>;
+export function merge<S extends Schema[]>(
+  schemasOrOptions: [...S] | MergeOptions<S>
+) {
+  const schemas = Array.isArray(schemasOrOptions)
+    ? schemasOrOptions
+    : schemasOrOptions.schemas;
+
+  let name = schemas.length > 0 ? schemas[0].title! : "";
+  if ("name" in schemasOrOptions && schemasOrOptions.name) {
+    name = schemasOrOptions.name;
+  }
+  return Type.Composite(schemas, { title: name });
+}
--- packages/api/src/schema/properties.ts
+++ packages/api/src/schema/properties.ts
@@ -27,12 +27,23 @@ TypeRegistry.Set(
   (schema: EnumProperty<string>, value) =>
     typeof value === "string" && schema.enum.includes(value)
 );
-export function enumProperty<T extends string[]>(values: [...T]) {
+
+export function enumProperty<A extends string[]>(
+  values: [...A]
+): EnumProperty<[...A][number]>;
+export function enumProperty<E extends Record<string, string>>(
+  values: E
+): EnumProperty<E[keyof E]>;
+export function enumProperty<T extends string[] | Record<string, string>>(
+  values: T
+) {
+  const arr = Array.isArray(values) ? values : Object.values(values);
+  const enumArr = [...new Set(arr)];
   return Type.Unsafe({
     [Kind]: "Enum",
-    enum: values,
-    default: values[0],
-  }) as EnumProperty<[...T][number]>;
+    enum: enumArr,
+    default: enumArr[0],
+  });
 }
 
 export { Kind } from "@sinclair/typebox";
